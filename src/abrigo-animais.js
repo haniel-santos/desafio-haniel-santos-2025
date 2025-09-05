c483d8fd43c60f2afd8303783c1423d833c6f4ee
haniel <hanielsilvasantos3@gmail.com>
2025-09-05 14:03:18 -0300

    feature : validação de animais validos

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,22 @@
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    const brinquedos = ['RATO', 'BOLA', 'LASER', 'CAIXA', 'NOVELO', 'SKATE'];
+    const listaPessoa1 = brinquedosPessoa1.split(',');
+    const listaPessoa2 = brinquedosPessoa2.split(',');
+    const listaAnimais = ordemAnimais.split(',');
+
+    const animaisValidos = [
+    { nome: 'Rex', especie: 'cão', brinquedos: ['RATO', 'BOLA'] },
+    { nome: 'Mimi', especie: 'gato', brinquedos: ['BOLA', 'LASER'] },
+    { nome: 'Fofo', especie: 'gato', brinquedos: ['BOLA', 'RATO', 'LASER'] },
+    { nome: 'Zero', especie: 'gato', brinquedos: ['RATO', 'BOLA'] },
+    { nome: 'Bola', especie: 'cão', brinquedos: ['CAIXA', 'NOVELO'] },
+    { nome: 'Bebe', especie: 'cão', brinquedos: ['LASER', 'RATO', 'BOLA'] },
+    { nome: 'Loco', especie: 'jabuti', brinquedos: ['SKATE', 'RATO'] }
+  ];
+  
+
   }
 }
 
